00b221768e43b8f0feb3386e8805a5ee015a53d8
Flylink <F.orch@mail.ru>
2026-01-22 21:00:14 +0700

    init Spring Boot backend with core domain and config

---
+++ backend/src/main/java/pro/cloudyprotection/CloudyProtectionApplication.java
@@ -0,0 +1,12 @@
+package pro.cloudyprotection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudyProtectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudyProtectionApplication.class, args);
+    }
+}
